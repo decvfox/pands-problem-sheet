d29b183a05f18f4f4538a5f313544a7fec470427
decvfox <10021067@atu.ie>
2023-02-18 19:25:02 +0000

    Updated to read amounts as integers

--- Bank.py
+++ Bank.py
@@ -2,7 +2,7 @@
 # Reads in 2 amounts, adds them, and then prints the answer
 # author Declan Fox
 
-amount1 = input("Enter amount1: ")
-amount2 = input("Enter amount2: ")
-sum = (int(amount1) + int(amount2)) / 100
-print(f'The sum of these is €{sum}')
+amount1 = int(input("Enter amount1: "))
+amount2 = int(input("Enter amount2: "))
+sum = amount1 + amount2
+print(f'The sum of these is €{(sum / 100)}')
