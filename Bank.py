2d0efa30a672927a909ab5bd3ec95902e5c514e1
decvfox <10021067@atu.ie>
2023-02-18 19:07:49 +0000

    week 02

---
+++ Bank.py
@@ -0,0 +1,8 @@
+# Bank.py
+# Reads in 2 amounts, adds them, and then prints the answer
+# author Declan Fox
+
+amount1 = input("Enter amount1: ")
+amount2 = input("Enter amount2: ")
+sum = (int(amount1) + int(amount2)) / 100
+print(f'The sum of these is €{sum}')
